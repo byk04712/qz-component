b79ad36f77de30b9c29bf7122ca14043711cce8c
qinzhen09 <739694218@qq.com>
2021-11-12 12:35:34 +0800

    feat: 加入 gulp 构建样式

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Vue2.x + Vite2.x + TypeScript 基于 Ant Design for Vue 封装通用组件库
+
+### 目录结构
+```
+├── build # 编译脚本
+├── dist # 打包后的代码
+├── coverage # 覆盖率报告
+├── examples # 代码范例
+├── node_modules
+├── packages # 组件代码
+├── scripts # 脚本 发布、提交信息检查
+├── src # 通⽤代码
+├── test # 测试
+└── types # TS类型定义
+```
--- packages/form/src/form.vue
+++ packages/form/src/form.vue
@@ -2,12 +2,12 @@
  * @Author: 秦真
  * @Date: 2021-11-10 15:14:27
  * @LastEditors: Do not edit
- * @LastEditTime: 2021-11-10 15:20:43
+ * @LastEditTime: 2021-11-11 23:08:13
  * @Description: 表单
- * @FilePath: \admin-fronted\bgy-component\packages\form\src\form.vue
+ * @FilePath: /admin-fronted/bgy-component/packages/form/src/form.vue
 -->
 <template>
-  <div>表单</div>
+  <div class="bgy-form">表单</div>
 </template>
 
 <script>
